99eca1185d24afeabec4a724696b1202985db63f
kubota@kubota-lab-desktop <kubotake3111@gmail.com>
2026-04-12 20:41:51 +0900

    modified sweep_rl_lora.sh

--- sweep_rl_lora.sh
+++ sweep_rl_lora.sh
@@ -2,8 +2,8 @@
 # lora_r × lr のグリッドサーチ
 # vLLMサーバは起動済みを前提とする
 
-LR_VALUES=(1e-5 3e-5 1e-4 3e-4)
-LORA_R_VALUES=(64 128 256)
+LR_VALUES=(1e-5 3e-5 1e-4)
+LORA_R_VALUES=(1 4 16 64 128 256)
 
 MODEL_ID="Qwen/Qwen2.5-1.5B-Instruct"
 
